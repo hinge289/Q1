8c9ccc72652d302b5a03127f22db0b0e8fa2bd16
tejas <tejas@BANDYAA>
2025-03-04 11:49:39 +0530

    Add project files.

---
+++ Test 04-03-25/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace Test_04_03_25
+{
+    public class Student:IComparable
+    {
+        public int Roll { get; set; }
+        public string Name { get; set; }
+        public double TotalMarks { get; set; }
+       
+    
+            public void Show()
+        {
+            
+            Console.WriteLine($"{Name}   {TotalMarks}   {Roll}");
+        }
+        
+
+        public int CompareTo(object? obj)
+        {
+            Student S1 = this;
+            Student temp = (Student)obj;
+            if (temp.TotalMarks < S1.TotalMarks)
+            {
+                return -1;
+            }
+            else if (temp.TotalMarks > S1.TotalMarks)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many Students Data you want to Enter");
+            int n = int.Parse(Console.ReadLine());
+            ArrayList student = new ArrayList();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter Roll No, Name, Total marks");
+                student.Add(new Student()
+                {
+                    Roll = int.Parse(Console.ReadLine()),
+                    Name = Console.ReadLine(),
+                    TotalMarks = double.Parse(Console.ReadLine())
+
+                });
+            }
+            student.Sort();
+            foreach (var item in student)
+            {
+                Student studentqw = item as Student;
+                studentqw.Show();
+            }
+        }
+    }
+}
